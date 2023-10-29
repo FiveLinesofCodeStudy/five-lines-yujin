c963b05c2d561274b6b7593f4a6e3bfe550e06d7
uuuuujin <yearn8546@gmail.com>
2023-10-29 14:18:34 +0900

    3.4 좋은 함수 이름의 속성

--- index.ts
+++ index.ts
@@ -81,6 +81,11 @@ function moveVertical(dy: number) {
 }
 
 function update() {
+  handleInputs();
+  updateMap();
+}
+
+function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
     if (current === Input.LEFT)
@@ -92,15 +97,17 @@ function update() {
     else if (current === Input.DOWN)
       moveVertical(1);
   }
+}
 
+function updateMap() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
       if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-        && map[y + 1][x] === Tile.AIR) {
+          && map[y + 1][x] === Tile.AIR) {
         map[y + 1][x] = Tile.FALLING_STONE;
         map[y][x] = Tile.AIR;
       } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-        && map[y + 1][x] === Tile.AIR) {
+          && map[y + 1][x] === Tile.AIR) {
         map[y + 1][x] = Tile.FALLING_BOX;
         map[y][x] = Tile.AIR;
       } else if (map[y][x] === Tile.FALLING_STONE) {
@@ -113,12 +120,7 @@ function update() {
 }
 
 function draw() {
-  let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
-  let g = canvas.getContext("2d");
-
-  g.clearRect(0, 0, canvas.width, canvas.height);
-
-  // 메서드 추출
+  const g = createGraphics();
   drawMap(g);
   drawPlayer(g);
 }
@@ -143,6 +145,13 @@ function drawMap(g: CanvasRenderingContext2D) {
         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
     }
   }
+}
+
+function createGraphics() {
+  let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
+  let g = canvas.getContext("2d");
+  g.clearRect(0, 0, canvas.width, canvas.height);
+  return g
 }
 
 function drawPlayer(g: CanvasRenderingContext2D) {
